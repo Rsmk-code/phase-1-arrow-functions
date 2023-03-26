ba629cbd028a5321324b6add3f7fd8672ca2f81e
Ahmad Ramin Soleyman kheyl <sramin963@gmail.com>
2023-03-26 21:26:57 +0300

    Assignment is done

---
+++ index.js
@@ -0,0 +1,32 @@
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(3,6));
+ const divide =(x,y) => x/y;
+ console.log(divide(2000, 100));
+
+ const square = x => x*x;
+ console.log(square(3));
+
+ const add = (x,y) => x+y;
+ console.log(add(3, 6));
+
+//  const twoAdder = x => x+2;
+//  console.log(twoAdder(3));
+
+//  const sum = function(P1, P2){
+//     console.log(`Adding ${P1}`);
+//     console.log(`Adding ${P2}`);
+//     return P1 + P2;
+//  }
+//  console.log(sum(9, 6));
+//  const nums = [1,2,3];
+//  const squares = nums.map(x => x**2);
+//  console.log(squares);
+//  console.log(nums);
+
+
+//  finishedItems = overdueTodoItems.map( item => item.className = "complete");
+//  header.innerText = `You finished ${finishedItems.length} items!`;
+//  finishedItems = overdueTodoItems.map( item => item.className = "complete" );
+//  header.innerText = `You finished ${finishedItems.length} items!`;
